ec7add0cdf3f9cc18774e17770299ca42a3e2991
John Gleezowood <psyriccio@gmail.com>
2016-08-08 23:28:20 +0300

    NEED impl

--- src/c1exchangegen/mapping/MappingNode.java
+++ src/c1exchangegen/mapping/MappingNode.java
@@ -19,6 +19,7 @@ import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import javax.swing.tree.TreeNode;
 import org.slf4j.LoggerFactory;
 
@@ -157,26 +158,46 @@ public class MappingNode implements TreeNode, NodeStateContainer {
             this.setState(NodeState.Warning);
         }
 
+        List<String> alrNeeded = new ArrayList<>();
+        
         this.inObject.getTypeReferences().stream()
+                .flatMap(
+                        (tRef) -> (tRef.getObjClass() == MetaObjectClass.TypeDescription 
+                                ? tRef.asTypeDescription().getTypes().stream()
+                                : tRef.getObjClass() == MetaObjectClass.Type
+                ? Stream.of(tRef.asType()) : Stream.of(tRef)))
                 .filter((tRef) -> tRef.asType().getName().contains("."))
                 .filter(
                         (tRef) -> MappingContext.MAPPING.getMaps().stream()
                         .noneMatch(
                                 (map) -> map.getIn().equals(tRef.getFullName()))
                 ).forEach((tRef) -> {
-                    infoChilds.add(
-                            new MappingInfoNode(this, "!NEED", tRef.asType().getName(), NodeState.Error));
+                    String typeName = tRef.asType().getName();
+                    if(!alrNeeded.contains(typeName)) {
+                        infoChilds.add(
+                                new MappingInfoNode(this, "!NEED", typeName, NodeState.Error));
+                        alrNeeded.add(typeName);
+                    }
                 });
 
         this.outObject.getTypeReferences().stream()
+                .flatMap(
+                        (tRef) -> (tRef.getObjClass() == MetaObjectClass.TypeDescription 
+                                ? tRef.asTypeDescription().getTypes().stream()
+                                : tRef.getObjClass() == MetaObjectClass.Type
+                ? Stream.of(tRef.asType()) : Stream.of(tRef)))
                 .filter((tRef) -> tRef.asType().getName().contains("."))
                 .filter(
                         (tRef) -> MappingContext.MAPPING.getMaps().stream()
                         .noneMatch(
                                 (map) -> map.getOut().equals(tRef.getFullName()))
                 ).forEach((tRef) -> {
-                    infoChilds.add(
-                            new MappingInfoNode(this, "!NEED", tRef.asType().getName(), NodeState.Error));
+                    String typeName = tRef.asType().getName();
+                    if(!alrNeeded.contains(typeName)) {
+                        infoChilds.add(
+                                new MappingInfoNode(this, "!NEED", typeName, NodeState.Error));
+                        alrNeeded.add(typeName);
+                    }
                 });
 
         this.infoChilds.forEach((inf) -> {
